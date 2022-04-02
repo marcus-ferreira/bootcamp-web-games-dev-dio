10fd0ad94e1ad5bd7c40d3b4fb7e7ecb362149af
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2022-04-02 19:32:05 -0300

    Add genius

---
+++ genius/script.js
@@ -0,0 +1,102 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+// 0 = green, 1 = red, 2 = yellow, 3 = blue
+
+const green = document.querySelector('.green');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
+
+// Cria a order aleatória de cores
+let shuffleOrder = () => {
+	let colorOrder = Math.floor(Math.random() * 4);
+	order[order.length] = colorOrder;
+	clickedOrder = [];
+
+	for (let i in order) {
+		let elementColor = createColorElement(order[i]);
+		lightColor(elementColor, Number(i) + 1);
+	}
+}
+
+// Acende a próxima cor
+let lightColor = (element, number) => {
+	number = number * 500;
+	setTimeout(() => {
+		element.classList.add("selected");
+	}, tempo - 250);
+	setTimeout(() => {
+		element.classList.remove("selected");
+	});
+}
+
+// Checa se os botões clicados são os mesmos da ordem gerada no jogo
+let checkOrder = () => {
+	for (let i in clickedOrder) {
+		if (clickedOrder[i] != order[i]) {
+			gameOver();
+			break;
+		}
+	}
+
+	if (clickedOrder.length == order.length) {
+		alert(`Pontuação: ${score}\nVocê acertou! Iniciano próximo nível!`);
+		nextLevel();
+	}
+}
+
+// Função para o clique do usuário
+let click = (color) => {
+	clickedOrder[clickedOrder.length] = color;
+	elementColor(color).classList.add("selected");
+
+	setTimeout(() => {
+		elementColor(color).classList.remove("selected");
+		checkOrder();
+	}, 250);
+}
+
+// Função que retorna a cor
+let createColorElement = (color) => {
+	if (color == 0) {
+		return green;
+	} else if (color == 1) {
+		return red;
+	} else if (color == 2) {
+		return yellow;
+	} else if (color == 3) {
+		return blue;
+	}
+}
+
+// Função para o próximo nível
+let nextlevel = () => {
+	score++;
+	shuffleOrder();
+}
+
+// Função para game over
+let gameOver = () => {
+	alert(`Pontuação: ${score}\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+	order = [];
+	clickedOrder = [];
+
+	playGame();
+}
+
+// Função para iniciar o jogo
+let playGame = () => {
+	alert(`Bem vindo ao Genius!\nIniciando um novo jogo!`);
+	score = 0;
+
+	nextlevel();
+}
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+playGame();
