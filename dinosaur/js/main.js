aedd7853dd55dbd8293243355e8a97be3cd038d8
Marcus Ferreira <mv.ferreirapinto@gmail.com>
2022-04-06 03:39:11 -0300

    Fix load img

--- dinosaur/js/main.js
+++ dinosaur/js/main.js
@@ -1,27 +1,3 @@
-class Player {
-	constructor() {
-		this.x = 40;
-		this.y = canvas.height - 60;
-		this.width = 60;
-		this.height = 60;
-		this.sprite = new Image();
-		this.sprite.src = '../art/dino.png';
-		this.vSpeed = 0;
-		this.jumpForce = 16;
-		this.isJumping = false;
-	};
-
-	draw() {
-		ctx.drawImage(this.sprite, this.x, this.y);
-	};
-
-	jump() {
-		this.isJumping = true;
-		this.vSpeed -= this.jumpForce;
-	};
-}
-
-
 class Cactus {
 	constructor() {
 		this.x = canvas.width;
@@ -42,22 +18,62 @@ class Cactus {
 	};
 }
 
+// Game settings
+const body = document.querySelector('body');
+body.style.overflow = 'hidden';
+body.style.backgroundColor = 'black';
 
-class Background {
-	constructor() {
-		this.x = 0;
-		this.y = 0;
-		this.width = 525;
-		this.height = 200;
-		this.sprite = new Image();
-		this.sprite.src = '../art/background.png';
-		this.speed = 4;
-	};
+const canvas = document.querySelector('canvas');
+canvas.style.backgroundColor = 'white';
+canvas.width = 16 * 55;
+canvas.height = 9 * 55;
+const canvasAspectRatio = canvas.width / canvas.height;
+
+const ctx = canvas.getContext('2d');
+const gravity = 0.8;
+
+// Variables
+let keys = {" ": false};
+let cacti = [];
+let cactusSpawnTime = 100;
+let score = 0;
+let highscore = 0;
+let time = 0;
+let gameState = 'title';
+
+const player = {
+	x: 40,
+	y: canvas.height - 60,
+	width: 60,
+	height: 60,
+	sprite: new Image(),
+	vSpeed: 0,
+	jumpForce: 16,
+	isJumping: false,
+
+	draw() {
+		ctx.drawImage(this.sprite, this.x, this.y);
+	},
+
+	jump() {
+		this.isJumping = true;
+		this.vSpeed -= this.jumpForce;
+	},
+};
+player.sprite.src = '../art/dino.png';
+
+const background = {
+	x: 0,
+	y: 0,
+	width: 525,
+	height: 200,
+	sprite: new Image(),
+	speed: 4,
 
 	draw() {
 		ctx.drawImage(this.sprite, this.x, 0, canvas.width, canvas.height);
 		ctx.drawImage(this.sprite, this.x + canvas.width, 0, canvas.width, canvas.height);
-	};
+	},
 
 	move() {
 		if (this.x <= -canvas.width) {
@@ -65,40 +81,21 @@ class Background {
 		} else {
 			this.x -= this.speed;
 		}
-	};
+	},
 }
+background.sprite.src = '../art/background.png';
 
 
-// Game settings
-const body = document.querySelector('body');
-body.style.backgroundColor = 'black';
-
-const canvas = document.querySelector('canvas');
-canvas.style.backgroundColor = 'white';
-canvas.width = 16 * 55;
-canvas.height = 9 * 55;
-
-const ctx = canvas.getContext('2d');
-const gravity = 0.8;
-const player = new Player();
-const background = new Background();
-const keys = {};
-let cacti = [];
-
-
-// Variables
-let score = 0;
-let highscore = 0;
-let time = 0;
-let gameState = 'title';
-
-
-// Initialize game
-titleScreen();
-
-
-addEventListener('keydown', event => { keys[event.key] = true; });
-addEventListener('keyup', event => { keys[event.key] = false; })
+addEventListener('keydown', event => { 
+	if (event.key in keys) {
+		keys[event.key] = true;
+	}
+});
+addEventListener('keyup', event => { 
+	if (event.key in keys) {
+		keys[event.key] = false;
+	}
+});
 
 // Draw title screen
 function titleScreen() {
@@ -120,7 +117,6 @@ function titleScreen() {
 	}, 1000 / 60);
 }
 
-
 // Initialize game
 function init() {
 	player.y = canvas.height - 60;
@@ -128,6 +124,7 @@ function init() {
 	player.isJumping = false;
 	background.x = 0;
 	cacti = [];
+	cactusSpawnTime = 100;
 	score = 0;
 	time = 0;
 	gameState = 'play';
@@ -139,26 +136,25 @@ function init() {
 	gameLoop();
 }
 
-
 // Game loop
 function gameLoop() {
 	if (gameState === 'play') {
 		// Increments timer
 		time++;
-	
+
 		// Clear canvas
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-	
+
 		// Draw sprites
 		background.draw();
 		player.draw();
-	
+
 		// Draw score
 		score = parseInt(time / 10);
 		ctx.font = "30px Arial";
 		ctx.textAlign = 'left';
 		ctx.fillText(`Score: ${score}`, 10, 30);
-	
+
 		// Draw highscore
 		if (score > highscore) {
 			highscore = score;
@@ -169,10 +165,19 @@ function gameLoop() {
 
 		// Move background
 		background.move();
-	
+
 		// Create cactus on random time
-		if (time % 100 == 0) {
-			createCactus();
+		if (time % cactusSpawnTime === 0) {
+			let random = Math.floor(Math.random() * 3);
+			if (random === 0) {
+				cacti.push(new Cactus());
+			}
+			console.log(random);
+		}
+
+		// Decrease cactus spawn time
+		if (time % 100 === 0) {
+			cactusSpawnTime--;
 		}
 	
 		// Draw and move cactus
@@ -182,19 +187,18 @@ function gameLoop() {
 			} else {
 				cactus.draw();
 				cactus.move();
-		
+
 				// Check collision with cactus
 				if (isColliding(player, cactus)) {
 					gameOver();
 				}
 			}
 		});
-		
+
 		requestAnimationFrame(gameLoop);
 	} else {
 		cancelAnimationFrame(gameLoop);
 	}
-
 
 	// Player jump
 	if (keys[' '] && !player.isJumping) {
@@ -214,13 +218,6 @@ function gameLoop() {
 	}
 }
 
-// Create cactus
-function createCactus() {
-	const cactus = new Cactus();
-	cacti.push(cactus);
-	return cactus;
-}
-
 // Check collision with cactus
 function isColliding(obj1, obj2) {
 	return obj1.x + obj1.width >= obj2.x &&
@@ -228,21 +225,20 @@ function isColliding(obj1, obj2) {
 		obj1.y + obj1.height >= obj2.y;
 }
 
-
 // Game over
 function gameOver() {
 	gameState = 'gameOver';
 
 	// Save highscore
 	window.localStorage.setItem('highscore', highscore);
-	
+
 	// Draw game over text
 	ctx.textAlign = 'center';
 	ctx.font = '60px Arial';
 	ctx.fillText('GAME OVER', canvas.width / 2, canvas.height / 2);
 	ctx.font = '30px Arial';
 	ctx.fillText(`Press space to replay`, canvas.width / 2, canvas.height / 2 + 60);
-	
+
 	// Start game
 	let startgame = setInterval(() => {
 		if (keys[' ']) {
@@ -252,3 +248,6 @@ function gameOver() {
 		}
 	}, 1000 / 60);
 }
+
+// Initialize game
+titleScreen();
